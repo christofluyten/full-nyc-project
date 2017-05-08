3cf6971830feaea62200260ec4eec7d455a95aaa
Christof Luyten <christof.luyten@student.kuleuven.be>
2017-05-08 18:24:18 +0200

    configs

--- src/main/java/com/github/christofluyten/experiment/NycExperiment.java
+++ src/main/java/com/github/christofluyten/experiment/NycExperiment.java
@@ -60,7 +60,7 @@ public class NycExperiment {
 	private final static ObjectiveFunction objFunc = Gendreau06ObjectiveFunction.instance(70);
 	private final static boolean enableReauctions = true;
 	private final static boolean computationsLogging = false;
-	private final static boolean ridesharing = true;
+	private final static boolean ridesharing = false;
 	private static String attribute = "noRidesharing";
 	private static boolean debug = true;
 	private static boolean gui = true;

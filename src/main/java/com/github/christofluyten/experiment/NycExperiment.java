10c7af9e48014a80fdb1c3b2d0f26a45cc1ea4d1
Christof Luyten <christof.luyten@student.kuleuven.be>
2017-05-15 17:59:58 +0200

    configs

--- src/main/java/com/github/christofluyten/experiment/NycExperiment.java
+++ src/main/java/com/github/christofluyten/experiment/NycExperiment.java
@@ -64,9 +64,9 @@ public class NycExperiment {
 	private static String attribute = "noRidesharing";
 	private static boolean debug = true;
 	private static boolean gui = true;
-	private final static int amountFilter = 1;
+	private final static int amountFilter = 5;
 	private static final int minNbOfBidders = 5;
-	private static int repetitions = 1;
+	private static int repetitions = 10;
 
 
 
